4c7421f409eb3155456b6295e951f6ec1f7fe51b
Vivekachooz <vivekachooz@gmail.com>
2023-08-20 20:49:17 +0530

    hackneyed: Add projects page
    Change-Id: Ie9f0e5c3c860ce53c8aacff719b2adca5f34fec3

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// JavaScript code goes here
-console.log("Hello from script.js!");
+// Get the projects section, home content, and the projects link from the header
+const projectsSection = document.getElementById("projects");
+const homeContent = document.getElementById("home-content");
+const projectsLink = document.getElementById("projects-link");
+
+// Add a click event listener to the projects link
+projectsLink.addEventListener("click", function () {
+    // Hide the home content and show the projects section
+    homeContent.style.display = "none";
+    projectsSection.style.display = "block";
+});
+
